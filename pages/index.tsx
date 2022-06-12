6adf84a64283269afbbce0d1ff9a751cce980237
mhmdho <hadiomidm@gmail.com>
2022-06-12 16:41:33 +0430

    Clean code: define style const

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 
+const style = {
+  wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between',
+}
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className={style.wrapper}>
       <Head>
         <title>TradingHills</title>
         <link rel="icon" href="/CLogo.svg" />
