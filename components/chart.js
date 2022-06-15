1c0141138d98ad0e548de7843e096c187e42f6cc
mhmdho <hadiomidm@gmail.com>
2022-06-15 18:00:54 +0430

    chart.js: Use ComboChart to draw both candlestick and scatter chart in one window

--- components/chart.js
+++ components/chart.js
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 import Chart from "react-google-charts";
+
+
 const data = [
-  ['day', 'a', 'b', 'c', 'd'],
-  ['Mon', 20, 28, 38, 45],
-  ['Tue', 31, 38, 55, 66],
-  ['Wed', 50, 55, 77, 80],
-  ['Thu', 77, 77, 66, 50],
-  ['Fri', 68, 66, 22, 15],
-];
+  ['day', 'dot', 'Low', 'open', 'close', 'high'],
+  ['Mon', null, 20, 28, 38, 45],
+  ['Tue', 31, 31, 38, 55, 66],
+  ['Wed', null, 50, 55, 77, 90],
+  ['Thu', 77, 77, 77, 66, 50],
+  ['Fri', 15, 68, 66, 22, 15],
+      ];
 class GoogleChart extends Component {
-  
+
   constructor(props) {
     super(props)
   }
   render() {
       return (
           <div className="container mt-5">
-              <h2>React Candlestick Chart Example</h2>
+              <h2>Bitcoin Candlestick Chart</h2>
               <Chart
                 width={'100%'}
                 height={450}
-                chartType="CandlestickChart"
+                chartType="ComboChart"
                 loader={<div>Loading Chart</div>}
                 data={data}
                 options={{
+                  title: 'Bitcoin',
+                  vAxis: { title: 'Price' },
+                  hAxis: { title: 'Date' },
+                  seriesType: 'candlesticks',
+                  series: { 1: { type: 'candlesticks', color: 'blue'},
+                            0: { type: 'scatter', pointShape: 'diamond', color: '#0f9d58', pointSize: 5 },
+                            // 0: { type: 'scatter', pointShape: 'triangle', color: '#a52714' },
+                          },
                   legend: 'none',
                 }}
                 rootProps={{ 'data-testid': '1' }}
-              />             
-          </div>                  
-      )
+              />
+          </div>
+    )
   }
 }
 export default GoogleChart;
