5de999a452fc86b6cfeeffe17ecead87244745e7
mhmdho <hadiomidm@gmail.com>
2022-06-24 23:35:19 +0430

    Add chart2.js in components and import it in main.js

--- components/main.js
+++ components/main.js
@@ -1,14 +1,16 @@
-import GoogleChart from '../components/chart'
+// import GoogleChart from '../components/chart'
+import ApexChart from '../components/chart2'
 
 
 const style = {
-  mainStyle: `flex w-full flex-1 flex-col items-center justify-center px-20 text-center`,
+  mainStyle: `flex-11 w-full flex-1 flex-col items-center justify-center px-2 text-center`,
 }
 
 const Main = () => {
     return (
       <main className={style.mainStyle}>
-          <GoogleChart />
+        {/* <GoogleChart /> */}
+        <ApexChart />
       </main>
     )
 }
