1eba1d4fc3d1ee7ea429a6d5e63e9f0226f1c157
mhmdho <hadiomidm@gmail.com>
2022-06-26 17:14:10 +0430

    Index.tsx: Slove ReferenceError of ApexChart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
 import type { NextPage } from 'next'
 import MyHead from '../components/head'
-import Main from '../components/main'
 import Footer from '../components/footer'
+import dynamic from 'next/dynamic'
+
+
+const DynamicChartWithNoSSR = dynamic(
+  () => import('../components/main'),
+  { ssr: false }
+)
 
 
 const style = {
   wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between',
 }
 
+
 const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
       <MyHead />
-      <Main />
+      <DynamicChartWithNoSSR />
       <Footer />
     </div>
   )
 }
+
 
 export default Home
