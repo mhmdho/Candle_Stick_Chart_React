a40834a1bebf79d30ecc94ac07525dc47fca5084
mhmdho <hadiomidm@gmail.com>
2022-07-22 23:54:29 +0430

    pointsdata.js: Get auxiliary points data from api

--- components/pointsdata.js
+++ components/pointsdata.js
@@ -3,6 +3,8 @@ import axios from "axios"
 
 const URL_low ="http://127.0.0.1:8000/lowpointsdata/?format=json"
 const URL_high ="http://127.0.0.1:8000/highpointsdata/?format=json"
+const URL_low_a ="http://127.0.0.1:8000/lowpointsdataaux/?format=json"
+const URL_high_a ="http://127.0.0.1:8000/highpointsdataaux/?format=json"
 
 
 function LowPointsDataFormat(data) {
@@ -21,6 +23,11 @@ async function lowPointsData() {
     return LowPointsDataFormat(response.data)
   }
 
+async function lowPointsDataAux() {
+  const response = await axios.get(URL_low_a);
+  return LowPointsDataFormat(response.data)
+}
+
 
 function HighPointsDataFormat(data) {
     return (
@@ -38,5 +45,9 @@ async function highPointsData() {
     return HighPointsDataFormat(response.data)
   }
 
+async function highPointsDataAux() {
+  const response = await axios.get(URL_high_a);
+  return HighPointsDataFormat(response.data)
+}
 
-export {lowPointsData, highPointsData};
+export {lowPointsData, highPointsData, lowPointsDataAux, highPointsDataAux};
