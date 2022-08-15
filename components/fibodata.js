5a4c8ee75f9dc59e2a520eaff82ae60aacc540cd
thehill <hadiomidm@gmail.com>
2022-08-15 15:13:51 +0430

    Edit fibodata.js to change style of 0 & 1 fibo lines

--- components/fibodata.js
+++ components/fibodata.js
@@ -7,6 +7,29 @@ const URL ="http://127.0.0.1:8000/fibodata/?format=json"
 function fiboDataFormat(data) {
     return (
       data.map(element => (
+        element.label == '0.00' || element.label == '1.00' ? 
+        {
+          y: element.fibo,
+          strokeDashArray: 7,
+          borderColor: 'gray',
+          fillColor: 'gray',
+          opacity: 0.2,
+          offsetX: '50%',
+          width: '6%',
+          label: {
+            borderColor: 'red',
+            borderRadius: 5,
+            orientation: 'vertical',
+            offsetY: 5,
+            offsetX: 90,
+            text: "[".concat(element.label).concat("] ", (parseFloat(element.fibo).toString())),
+            style: {
+              fontSize: '10px',
+              color: 'black',
+              background: 'gray',
+            },
+          }
+        } :
         {
           y: element.fibo,
           strokeDashArray: 10,
@@ -27,7 +50,7 @@ function fiboDataFormat(data) {
               color: 'black',
               background: 'orange',
             },
-          },
+          }
         }
         )
       ))
